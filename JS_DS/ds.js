6cb28deb1e9d8ba01c3012513d467517d23f04d4
b2ns <eminemdsp@163.com>
2017-09-21 23:33:26 +0800

    add Bag, Stack and Queue

--- JS_DS/ds.js
+++ JS_DS/ds.js
@@ -10,12 +10,18 @@
   var Class = function (attr,method,parent) {
     var $class=function (obj) {
       for(var i in attr){
-        this[i]=attr[i];
+        if(Object.prototype.toString.call(attr[i]) === "[object Array]"){
+          this[i]=new Array();
+        }else if(Object.prototype.toString.call(attr[i]) === "[object Object]"){
+          this[i]=new Object();
+        }else{
+          this[i]=attr[i];
+        }
       }
       for(var i in obj){
         this[i]=obj[i];
       }
-      if(typeof this.init === "function") this.init();
+      this.init();
     };
 
     if(typeof parent === "function"){
@@ -25,6 +31,7 @@
     }
 
     $class.fn=$class.prototype;
+    $class.fn.init=function (){console.log("init");};
     for(var i in method){
       $class.fn[i]=method[i];
     }
@@ -35,12 +42,60 @@
   //Bag
   ds.Bag=Class(
     {
-      size:0,
+      $array: []
     },
     {
-      setSize:function (size) {
-        this.size=size;
+      getSize:function () {
+        return this.$array.length;
       },
+      push:function (item) {
+        this.$array.push(item);
+      },
+      pick:function () {
+        return this.$array[Math.floor(Math.random()*this.getSize())];
+      },
+      find:function (item) {
+        return this.$array.indexOf(item)!==-1;
+      },
+    }
+  );
+
+  //Stack
+  ds.Stack=Class(
+    {
+      $array:[]
+    },
+    {
+      push:function (item) {
+        this.$array.push(item);
+      },
+      pop:function () {
+        return this.$array.pop();
+      },
+      top:function () {
+        return this.$array[this.$array.length-1];
+      }
+    }
+  );
+
+  //Queue
+  ds.Queue=Class(
+    {
+      $array:[]
+    },
+    {
+      enqueue:function (item) {
+        this.$array.push(item);
+      },
+      dequeue:function () {
+        return this.$array.shift();
+      },
+      first:function () {
+        return this.$array[0];
+      },
+      last:function () {
+        return this.$array[this.$array.length-1];
+      }
     }
   );
 
