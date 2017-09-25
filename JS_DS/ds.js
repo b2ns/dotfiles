651dad20a312ce0284176c3e2a427d56db08a0b2
b2ns <eminemdsp@163.com>
2017-09-25 17:38:29 +0800

    change class defination , remove Bag and add List

--- JS_DS/ds.js
+++ JS_DS/ds.js
@@ -7,55 +7,52 @@
   var ds=function(){};
 
   //make it easier to define a new class
-  var Class = function (attr,method,parent) {
-    var $class=function (obj) {
-      for(var i in attr){
-        if(Object.prototype.toString.call(attr[i]) === "[object Array]"){
-          this[i]=new Array();
-        }else if(Object.prototype.toString.call(attr[i]) === "[object Object]"){
-          this[i]=new Object();
-        }else{
-          this[i]=attr[i];
-        }
-      }
-      for(var i in obj){
-        this[i]=obj[i];
-      }
-      this.init();
+  var Class = function (method,parent) {
+    var _class=function () {
+      this._init.apply(this,arguments);
     };
 
     if(typeof parent === "function"){
       var tmp = function(){};
       tmp.prototype=parent.prototype;
-      $class.prototype=new tmp;
+      _class.prototype=new tmp;
+      _class.prototype.constructor=_class;
     }
 
-    $class.fn=$class.prototype;
-    $class.fn.init=function (){console.log("init");};
+    _class.fn=_class.prototype;
+    _class.fn._init=function (){};
     for(var i in method){
-      $class.fn[i]=method[i];
+      _class.fn[i]=method[i];
     }
-    return $class;
+    return _class;
   };
   ds.Class=Class;
 
-  //Bag
-  ds.Bag=Class(
+  //hashTable
+  ds.hashTable=Class(
     {
-      $array: []
-    },
-    {
-      getSize:function () {
-        return this.$array.length;
+      _init:function () {
+        this._table = new Object();
       },
-      push:function (item) {
-        this.$array.push(item);
+      value:function (key) {
+        return this._table[key];
       },
-      pick:function () {
-        return this.$array[Math.floor(Math.random()*this.getSize())];
+      insert:function (key,val) {
+        this._table[key]=val;
       },
-      find:function (item) {
-        return this.$array.indexOf(item)!==-1;
+      delete:function (key) {
+        delete this._table[key];
+      },
+      find:function (key) {
+        return this.value(key) !== undefined;
+      },
+      clear:function () {
+        this._table=new Object();
+      },
+      forEach:function (func) {
+        for(var key in this._table){
+          func.call(this,key,this._table[key]);
+        }
       },
     }
   );
@@ -63,17 +60,20 @@
   //Stack
   ds.Stack=Class(
     {
-      $array:[]
-    },
-    {
+      _init:function () {
+        this._array = new Array();
+      },
       push:function (item) {
-        this.$array.push(item);
+        this._array.push(item);
       },
       pop:function () {
-        return this.$array.pop();
+        return this._array.pop();
       },
       top:function () {
-        return this.$array[this.$array.length-1];
+        return this._array[this._array.length-1];
+      },
+      bottom:function () {
+        return this._array[0];
       }
     }
   );
@@ -81,24 +81,86 @@
   //Queue
   ds.Queue=Class(
     {
-      $array:[]
-    },
-    {
+      _init:function (size) {
+        this._array = new Array();
+      },
       enqueue:function (item) {
-        this.$array.push(item);
+        this._array.push(item);
       },
       dequeue:function () {
-        return this.$array.shift();
+        return this._array.shift();
       },
       first:function () {
-        return this.$array[0];
+        return this._array[0];
       },
       last:function () {
-        return this.$array[this.$array.length-1];
+        return this._array[this._array.length-1];
       }
     }
   );
 
+  //List
+  var ListNode=Class(
+    {
+      _init:function (val,pre,next) {
+        this.val=val;
+        this.pre=pre;
+        this.next=next;
+      }
+    }
+  );
+
+  ds.List=Class(
+    {
+      _init:function () {
+        this._head=new ListNode();
+        this._length=0;
+      },
+      length:function () {
+        return this._length;
+      },
+      insert:function (val,nodeVal) {
+        var node=(nodeVal)?this.find(nodeVal):this._head;
+        node=(node)?node:this._head;
+
+        var newNode=new ListNode(val,node,node.next);
+        if(node.next){
+          node.next.pre=newNode;
+        }
+        node.next=newNode;
+
+        this._length++;
+      },
+      delete:function (val) {
+        var node=this.find(val);
+        if(node){
+          node.pre.next=node.next;
+          node.next.pre=node.pre;
+        }
+
+        this._length--;
+      },
+      find:function (val) {
+        var node=this._head.next;
+        while(node){
+          if(node.val === val) break;
+          node=node.next;
+        }
+        return node;
+      },
+      clear:function () {
+        this._head=new ListNode();
+        this._length=0;
+      },
+      forEach:function (func) {
+        var node=this._head.next;
+        while(node){
+          func.call(this,node.val);
+          node=node.next;
+        }
+      },
+    }
+  );
 
   
   //expose the ds
