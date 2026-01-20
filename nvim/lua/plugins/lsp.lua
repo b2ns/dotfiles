85a9da1608770c928223f3f4b941d54aebdf3e44
b2ns <eminemdsp@163.com>
2026-01-20 14:14:27 +0800

    chore: dict completion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -23,4 +23,40 @@ return {
       })
     end,
   },
+
+  -- dictionary completion
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      {
+        "Kaiser-Yang/blink-cmp-dictionary",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+      -- ... Other dependencies
+    },
+    opts = {
+      sources = {
+        -- Add 'dictionary' to the list
+        -- default = { "dictionary", "lsp", "path", "luasnip", "buffer" },
+        default = { "lsp", "path", "buffer", "dictionary" },
+        providers = {
+          dictionary = {
+            module = "blink-cmp-dictionary",
+            name = "Dict",
+            -- Make sure this is at least 2.
+            -- 3 is recommended
+            min_keyword_length = 3,
+            max_items = 8,
+            -- sort it at the last
+            score_offset = -10,
+            opts = {
+              dictionary_files = {
+                vim.fn.expand("~/github/b2ns/dotfiles/nvim-resources/word.txt"),
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
